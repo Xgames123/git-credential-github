ab1c260517e5e7f562a9b9169e82f159723d6b4f
ldev <ldev@ldev.eu.org>
2023-12-03 10:45:56 +0100

    removed unused pack.sh

--- pack.sh
+++
@@ -1,9 +0,0 @@
-pkgver=$(cargo run -q -- --version | rg -o '[0-9]\.[0-9]\.[0-9]')
-
-echo Packaging gh-login-debian-armv7
-packaging/debian/pack.sh armv7-unknown-linux-gnueabihf $pkgver
-echo Packaging gh-login-debian-x86_64
-packaging/debian/pack.sh x86_64-unknown-linux-gnu $pkgver
-
-echo "Done"
-echo "Packages at target/packaging"
